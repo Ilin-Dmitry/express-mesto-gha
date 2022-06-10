5984f2e29ddfaf05824041300b544b088136f348
Dmitry Ilin <dm-ilin@mail.ru>
2022-06-10 22:28:33 +0400

    Добавить модели card и user

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
-
-
 
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на ${PORT} порту`)
+  console.log(`Сервер запущен на ${PORT} порту`);
 });
-
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  }
+})
+
+
+
+module.exports = mongoose.model('user', userSchema)
