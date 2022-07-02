fd5d875961824de37dc0949bf8be6537a1540157
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-02 13:58:38 +0400

    Создать валидацию параметра cardId

--- routes/cards.js
+++ routes/cards.js
@@ -5,8 +5,16 @@ const {
   showAllCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 router.get('/cards', showAllCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
@@ -14,6 +22,10 @@ router.post('/cards', celebrate({
     link: Joi.string().required().pattern(/https?:\/\/[a-z0-9-]+\.[\S]*/i),
   }),
 }), createCard);
-router.delete('/cards/:cardId', deleteCard);
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
 
 module.exports = router;
