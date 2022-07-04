b36cee7e3a3a9f8fcd72883e63541aecef14785f
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-04 18:15:48 +0400

    Исправить ошибку в auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,15 +5,12 @@ const errModule = require('../errors/handleError');
 module.exports = (req, res, next) => {
   const { _id } = req.cookies;
   if (!_id) {
-    // return res.status(401).send({ message: 'Необходима авторизация!' });
     throw new UnauthorizedError('Необходима авторизация!');
   }
-  const token = _id.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(_id, 'some-secret-key');
   } catch (err) {
-    // return res.status(401).send({ message: 'Необходима авторизация' });
     next(errModule.handleError(err, res));
   }
   req.user = payload;
