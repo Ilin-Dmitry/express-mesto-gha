426f70b1f1144d42af0af7d1c0266fa5f419dd2f
Dmitry Ilin <dm-ilin@mail.ru>
2022-07-04 19:53:27 +0400

    Переименовать _id в token

--- controllers/users.js
+++ controllers/users.js
@@ -99,7 +99,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      res.cookie('_id', token, {
+      res.cookie('token', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: true,
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,13 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const errModule = require('../errors/handleError');
 
 module.exports = (req, res, next) => {
-  const { _id } = req.cookies;
-  if (!_id) {
+  const { token } = req.cookies;
+  if (!token) {
     throw new UnauthorizedError('Необходима авторизация!');
   }
   let payload;
   try {
-    payload = jwt.verify(_id, 'some-secret-key');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     next(errModule.handleError(err, res));
   }
